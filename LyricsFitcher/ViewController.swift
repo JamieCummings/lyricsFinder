74400405581f6ef774edbab852d35788aa9e71b1
Jamie Cummings <jamiecummings@MacBook-Pro-2.local>
2018-10-23 10:22:56 -0500

    Initial Commit

---
+++ LyricsFitcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LyricsFitcher
+//
+//  Created by Jamie Cummings on 10/23/18.
+//  Copyright © 2018 Jamie Cummings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
